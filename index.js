6a134e031f75c60fe3bd3e78e8f682b4313c9b28
ben <ben@dreamerslab.com>
2014-05-25 16:06:55 +0800

    [new feature] ability to use model outside of the application scope

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var coke = require( './lib/boot' )
 coke.version = JSON.parse( fs.readFileSync( __dirname + '/package.json', 'utf8' )).version;
 coke.utils   = require( './lib/utils' );
 
+coke.setup_models = function ( base_dir, callback ){
+  require( './lib/global' )( base_dir );
+  require( CORE_DIR + '/logger' );
+  require( CORE_DIR + '/utils' );
+  require( CORE_DIR + '/model' )({
+    mongoose : require( 'mongoose' )
+  }, callback );
+};
+
 /**
  * Exports module.
  */
